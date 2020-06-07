379e05015b37515d1d33fc477ed7e01f4a719059
Uherda10 <davca.uher10@gmail.com>
2020-06-08 00:56:06 +0200

    Adjusted main page. Added graphical elements.

--- OGSatApp/OGSatApp.Android/MainActivity.cs
+++ OGSatApp/OGSatApp.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace OGSatApp.Droid
 {
-    [Activity(Label = "OGSatApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "OGSat App", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
--- OGSatApp/OGSatApp.Android/Resources/Resource.designer.cs
+++ OGSatApp/OGSatApp.Android/Resources/Resource.designer.cs
@@ -5792,10 +5792,13 @@ namespace OGSatApp.Droid
 			public const int OGSatLogo = 2131165300;
 			
 			// aapt resource value: 0x7F070075
-			public const int tooltip_frame_dark = 2131165301;
+			public const int OGSatLogoBackgroundOff = 2131165301;
 			
 			// aapt resource value: 0x7F070076
-			public const int tooltip_frame_light = 2131165302;
+			public const int tooltip_frame_dark = 2131165302;
+			
+			// aapt resource value: 0x7F070077
+			public const int tooltip_frame_light = 2131165303;
 			
 			static Drawable()
 			{
